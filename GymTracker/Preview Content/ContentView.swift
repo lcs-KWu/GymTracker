8c3628ddefaffeac5a364b8d0601c4583fb996bb
Kevin Wu <KWu@lcs.on.ca>
2025-05-26 16:08:09 -0400

    added the landingview, home page fot the app

--- GymTracker/Preview Content/ContentView.swift
+++ GymTracker/Preview Content/ContentView.swift
@@ -1,1 +1,45 @@
 
+//
+//  ContentView.swift
+//  GymTracker
+//
+//  Created by Yixuan Wu on 2025-05-11.
+//
+
+import SwiftUI
+import SwiftData
+
+struct LandingView: View {
+// MARK: Stored Properties
+    @Environment(\.modelContext) var modelContext
+    @Query var workoutPlans: [WorkoutPlan]
+    
+    // MARK: computed properties
+    var body: some View {
+        TabView {
+            
+            // Check and enter plans
+            DayView()
+                .tabItem {
+                    Image(systemName: "list.bullet.rectangle.portrait")
+                    Text("Plans")
+                }
+                .tag(1)
+            
+            // add plans
+            AddingView()
+                .tabItem {
+                    Image(systemName: "plus.circle.fill")
+                    Text("Add Plan")
+                }
+                .tag(2)
+        }
+        .preferredColorScheme(.dark)
+        .tint(.orange)
+    }
+}
+
+#Preview {
+    LandingView()
+        .modelContainer(for: [WorkoutPlan.self, WorkoutExercise.self])
+}
